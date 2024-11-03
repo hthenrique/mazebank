6eb5273d9e699712cc304b1246aea0a5c4f6d43c
Henrique Teixeira <ht.henrique@live.com>
2024-11-03 02:06:22 -0300

    fix: add cors config

--- src/main/java/ht/henrique/mazebank/controller/AuthenticateController.java
+++ src/main/java/ht/henrique/mazebank/controller/AuthenticateController.java
@@ -11,14 +11,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/authenticate")
 @Slf4j
+@CrossOrigin(origins = "*", methods = { RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE })
 public class AuthenticateController {
 
     @Autowired
